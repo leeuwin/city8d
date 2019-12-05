d8879eebfe98bed9236d66b20c92d35295c31457
lim <lim@xmsonyou.com>
2019-12-06 00:12:45 +0800

    修复一些细节

--- utils/config.js
+++ utils/config.js
@@ -2,8 +2,8 @@ class Config {
   contructor() {}
 }
 
-//Config.restUrl = 'https://api.ngsfc.cn/platform';
-//Config.restUrl = 'http://39.100.243.238/api';
+
 Config.restUrl = 'https://api.city8d.com/api';
+//Config.restUrl = 'http://api.city8d.com/api';
 
 export { Config }
